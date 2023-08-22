12901aae06e589865731fc31f9d2a5d5de8e69ae
AIRGG <>
2023-08-22 21:29:25 +0700

    coba lagi cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie', '*', 'api'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
