27c83187672af816161775f4346968018e158c32
AIRGG <>
2023-08-22 21:21:26 +0700

    put access control authorization

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
         // header('Access-Control-Allow-Origin:  *');
         // header('Access-Control-Allow-Origin:  http://localhost:3000');
         // header('Access-Control-Allow-Origin:  http://192.168.1.24:3000');
-        // header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         // header('Access-Control-Allow-Methods:  POST, PUT');
         return $next($request);
     }
